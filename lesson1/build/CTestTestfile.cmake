c523203159fabcb260b6e3d356e94934a0b09758
ocelot <ocelot@area.zov>
2024-09-05 16:22:01 +0300

    Changed structure of repo

---
+++ lesson1/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ocelot/programming/otus/otus-cpp/lesson1
+# Build directory: /home/ocelot/programming/otus/otus-cpp/lesson1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/home/ocelot/programming/otus/otus-cpp/lesson1/CMakeLists.txt;85;add_test;/home/ocelot/programming/otus/otus-cpp/lesson1/CMakeLists.txt;0;")
