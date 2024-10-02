a33b2a0569ac760a230fac40da6eaa13c1ba170b
ocelot <ocelot@area.zov>
2024-10-02 21:54:37 +0300

    Done Homework 4

--- main.cpp
+++ main.cpp
@@ -1,163 +1,117 @@
 #include <iostream>
+#include <bitset>
+#include <cmath>
 #include <vector>
-#include <memory>
-#include <map>
+#include <string>
+#include <algorithm>
+#include <list>
+#include <tuple>
 
-template<typename T, std::size_t MaxItems>
-class CustomAllocator {
-public:
+/// \brief Домашнее задание по курсу C++ Proffessional от OTUS
 
-    using value_type = T;
-    CustomAllocator() = default;
-    template<typename U, std::size_t S>
-    constexpr CustomAllocator(const CustomAllocator<U, S>&) noexcept {}
-
-    template<class Other>
-        struct rebind { using other =  CustomAllocator<Other, MaxItems>; };
-
-    T* allocate(std::size_t n) {
-        // std::cout << "Allocating " << n * sizeof(T) << " bytes" << std::endl;
-        if (n + size > MaxItems) {
-            // std::cout << "POOTIS\n";
-            throw std::bad_alloc();
-        }
-        size += n;
-        // std::cout << "NEW ALLOCATION "  << n << "\nCUR SIZE " << size << std::endl;
-        return static_cast<T*>(::operator new(n * sizeof(T)));
-    }
-    void deallocate(T* p, std::size_t n) noexcept {
-        // std::cout << "Deallocating memory" << std::endl;
-        ::operator delete(p);
-        size -= n;
-    }
-    template<typename U, typename... Args>
-    void construct(U* p, Args&&... args) {
-        // std::cout << "Constructing element" << std::endl;
-        new(p) U(std::forward<Args>(args)...);
-    }
-    template<typename U>
-    void destroy(U* p) noexcept {
-        // std::cout << "Destroying element" << std::endl;
-        p->~U();
-    }
-    friend bool operator==(const CustomAllocator&, const CustomAllocator&) { return true; }
-    friend bool operator!=(const CustomAllocator&, const CustomAllocator&) { return false; }
-private:
-    std::size_t size = 0;
-};
-
-template<typename T, typename A = std::allocator<T>>
-class CustomList
+namespace sfinae
 {
-public:
-    // using value_type = T;
-    typedef T value_type;
-    struct Node
-    {
-        typedef T value_type;
-        Node* next = nullptr;
-        T val;
-    };
-    typename A::template rebind<Node>::other nodeAlloc;
-    CustomList()
-    {
-        _begin = _end = nullptr;
-    }
-
-    void push_back(const T& val)
-    {
-        // typename A::template rebind<Node>::other nodeAlloc;
-        // Node* newNode = std::allocator_traits<Node>::allocate(nodeAlloc, 1);
-        Node* newNode = nodeAlloc.allocate(1);
-        if(_size == 0)
+    namespace ip_printer {
+        /**
+         * \brief Перегрузка функции для целочисленного кодирования IP-адреса. Декодируется в IP-адрес побайтово.
+         *
+         * \param address IP-адрес в виде целого числа
+         */
+        template <typename T>
+        auto print_ip(T address, long) -> decltype(abs(address))
         {
-            _begin = _end = newNode;
+            std::bitset<sizeof(T)*8> bs{(long long unsigned int)address};
+            uint16_t base, num;
+            for (size_t i = bs.size() - 1; i >= 7; i -= 8)
+            {
+                base = 1;
+                num = 0;
+                for (unsigned int j = i - 7; j <= i; j++)
+                {
+                    num += bs[j] * base;
+                    base *= 2;
+                }
+                std::cout << num;
+                if(i != 7)
+                    std::cout << ".";
+                else
+                    break;
+            }
+            std::cout << std::endl;
+            return 0;
         }
-        else
+
+        
+        /**
+         * \brief Перегрузка функции для строкового формата IP-адреса. Выводится в функции как есть
+         *
+         * \param address IP-адрес в виде строки
+         */
+        template <typename T>
+        auto print_ip(T address, int) -> decltype(address.c_str()) 
         {
-            _end->next = newNode;
-            _end = newNode;
+            std::cout << address << std::endl;
+            return address.c_str();
         }
-        // std::allocator_traits<Node>::construct(newNode, Node());
-        nodeAlloc.construct(newNode);
-        _end->val = val;
-        _end->next = nullptr;
-        _size++;
+
+        /**
+         * \brief Перегрузка функции для контейнерного формата IP-адреса. Элементы контейнера выводятся по очереди через точку.
+         *
+         * \param address IP-адрес в виде контейнера std::list или std::vector
+         */
+        template <typename T>
+        auto print_ip(T address, long) -> decltype(address.size())
+        {
+            size_t cont_s = address.size(), i = 0;
+            for(auto item:address)
+            {
+                std::cout << item;
+                if(i < cont_s - 1)
+                    std::cout << ".";
+                i++;
+            }
+            std::cout << std::endl;
+            return address.size();
+        }
+
+        // tuple
+        template <typename T = std::tuple<int>>
+        auto print_ip(T address, int) -> decltype(std::get<0>(address))
+        {
+            std::cout << "Shnyaga: " << address << std::endl;
+            // size_t cont_s = address.size(), i = 0;
+            // for(auto item:address)
+            // {
+            //     std::cout << item;
+            //     if(i < cont_s - 1)
+            //         std::cout << ".";
+            //     i++;
+            // }
+            // std::cout << std::endl;
+            // return address.size();
+        }
     }
 
-    Node* get_begin()
+    /*!
+    \brief Выводит входное входную переменную в виде IP адреса. 
+    \param[in] address Входной объект, содержащий IP адрес: целочисленный тип(побитовое преобразование), строка(вывод как есть), контейнер(вывод элементов)
+    */
+    template <typename T>
+    auto print_ip(T address)
     {
-        return _begin;
+        ip_printer::print_ip(address, int{});
     }
-
-    Node* get_end()
-    {
-        return _end;
-    }
-private:
-    Node* _begin;
-    Node* _end;
-    std::size_t _size = 0;
-    
-    A alloc;
-};
-
-int fact(int x)
-{
-    if(x < 1)
-        return 1;
-    int b = 1;
-    for (int i = 1; i <= x; i++)
-       b *= i;
-    return b;
-    
 }
 
 int main()
 {
-    int fact_arr[10];
-    std::map<int, int> m1;
-    for (int i = 0; i < 10; i++)
-    {
-        fact_arr[i] = fact(i);
-        m1[i] = fact_arr[i];
-    }
-    // for (int i = 0; i < 10; i++)
-    // {
-    //     std::cout << m1[i] << std::endl;
-    // }
-
-    std::map<int, int, std::less<int>, CustomAllocator<int, 10> > m2;
-    for (int i = 0; i < 10; i++)
-    {
-        m2[i] = fact_arr[i];
-    }
-    for (auto item : m2)
-    {
-        std::cout << item.first << " " << item.second << "\n";
-    }
-    
-    CustomList<int> lst1;
-    for (int i = 0; i < 10; i++)
-    {
-        lst1.push_back(fact_arr[i]);
-    }
-    // std::cout << "CustomList<int, std::allocator<int>>\n";
-    // for (auto item = lst1.get_begin(); item != nullptr; item = item->next)
-    // {
-    //     std::cout << item->val << std::endl;
-    // }
-    
-    CustomList<int, CustomAllocator<int, 10>> lst2;
-    for (int i = 0; i < 10; i++)
-    {
-        lst2.push_back(fact_arr[i]);
-    }
-    // std::cout << "CustomList<int, CustomAllocator<int, 10>>\n";
-    for (auto item = lst2.get_begin(); item != nullptr; item = item->next)
-    {
-        std::cout << item->val << std::endl;
-    }
+    sfinae::print_ip( int8_t{-1}); // 255
+    sfinae::print_ip( int16_t{0} ); // 0.0
+    sfinae::print_ip( int32_t{2130706433} ); // 127.0.0.1
+    sfinae::print_ip( int64_t{8875824491850138409} );// 123.45.67.89.101.112.131.41
+    sfinae::print_ip(std::string{"Hello, World!"}); // Hello, World!
+    sfinae::print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400
+    sfinae::print_ip( std::list<short>{400, 300, 200, 100} ); // 400.300.200.100
 
     return 0;
 }
